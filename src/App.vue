07baba874b201fe6f09f4c15eb2941ebcb6c2933
John Kelly <jkelly@incomm.com>
2022-03-11 14:37:54 -0600

    Updated for input fields

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,19 @@
       <div class="col-sm h4">Style Name</div>
       <div class="col-sm h4">Image Name</div>
       <div class="col-sm h4">Active</div>
+      <div class="col-sm"></div>
     </div>
 
     <div class="row text-start " v-for="stylecode in stylecodes" :key="stylecode.stylecode" >
-      <div class="col-sm">{{stylecode.stylecode}}</div>
-      <div class="col-sm">{{stylecode.stylename}}</div>
-      <div class="col-sm">{{stylecode.imagename}}</div>
-      <div class="col-sm">{{stylecode.isactive}}</div>
+      <div class="col-sm"><input type="text" class="form-control" v-model="stylecode.stylecode"/></div>
+      <div class="col-sm"><input type="text" class="form-control" v-model="stylecode.stylename" /></div>
+      <div class="col-sm"><input type="text" class="form-control" v-model="stylecode.imagename"/></div>
+      <div class="col-sm"><input type="text" class="form-control" v-model="stylecode.isactive"/></div>
+      <div class="col-sm"><!--<button class="btn btn-sm btn-outline-danger" v-on:click="deletethis(index)">Delete This</button>--></div>
     </div>
 
-    <div class="row  text-start ">
-      <div class="col-sm"><input class="form-control" type="text" value="000123"/></div>
-      <div class="col-sm">Not Stylish</div>
-      <div class="col-sm">Troll.png</div>
-      <div class="col-sm">TootsieRoll</div>
-    </div>
-    <button class="btn btn-outline-primary">Save</button>
+
+    <button class="btn btn-outline-primary">Add</button>
     <button class="btn btn-outline-danger" v-on:click="deletelast()">Delete Last</button>
 
 
@@ -46,6 +43,11 @@ export default {
   methods: {
     deletelast() {
       this.stylecodes.pop();
+    },
+    deletethis(index) {
+      console.log(index);
+      //this.stylecodes.slice(index, index+1);
+      delete this.stylecodes[index];
     }
   },
   data() {
