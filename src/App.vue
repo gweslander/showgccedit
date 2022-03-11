8e41e19533e1531370172173323506572e2d9122
John Kelly <jkelly@incomm.com>
2022-03-11 14:23:06 -0600

    Showing a sample javascript

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="container">
     <img alt="Vue logo" src="./assets/logo.png">
-    <h1 class="display-1">GCC Edit Replacement</h1>
+    <h1 class="display-4">GCC Edit Replacement</h1>
     <div class="row text-start ">
-      <div class="col-sm">Style Code</div>
-      <div class="col-sm">Style Name</div>
-      <div class="col-sm">Image Name</div>
-      <div class="col-sm">4th thing john forgets</div>
+      <div class="col-sm h4">Style Code</div>
+      <div class="col-sm h4">Style Name</div>
+      <div class="col-sm h4">Image Name</div>
+      <div class="col-sm h4">Active</div>
     </div>
-    <div class="row  text-start " v-for="stylecode in stylecodes" :key="stylecode.stylecode" >
+
+    <div class="row text-start " v-for="stylecode in stylecodes" :key="stylecode.stylecode" >
       <div class="col-sm">{{stylecode.stylecode}}</div>
       <div class="col-sm">{{stylecode.stylename}}</div>
       <div class="col-sm">{{stylecode.imagename}}</div>
       <div class="col-sm">{{stylecode.isactive}}</div>
-
     </div>
+
     <div class="row  text-start ">
       <div class="col-sm"><input class="form-control" type="text" value="000123"/></div>
       <div class="col-sm">Not Stylish</div>
@@ -22,17 +23,14 @@
       <div class="col-sm">TootsieRoll</div>
     </div>
     <button class="btn btn-outline-primary">Save</button>
+    <button class="btn btn-outline-danger" v-on:click="deletelast()">Delete Last</button>
 
 
     <div class="row">
       <div class="col">
-{{stuff}}
+{{stylecodes}}
       </div>
     </div>
-    
-
-    
-
   </div>
   <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
 </template>
@@ -45,12 +43,18 @@ export default {
   components: {
     //HelloWorld
   },
+  methods: {
+    deletelast() {
+      this.stylecodes.pop();
+    }
+  },
   data() {
     return { 
       stuff: [{sandwich: "subway"},{sandwich: "mike's"}],
       stylecodes: [
         {stylecode:"123", stylename:"Firetruck Red", imagename: "troll.png", isactive: "true"},
-        {stylecode:"456", stylename:"Boat Blue", imagename: "goblin.png", isactive: "true"}
+        {stylecode:"456", stylename:"Boat Blue", imagename: "goblin.png", isactive: "true"},
+        {stylecode:"679", stylename:"Sun Yellow", imagename: "goblin.png", isactive: "true"}
       ]
     };
   }
