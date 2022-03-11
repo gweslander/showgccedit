618ff82ff1151f74aa686f5610e73125cf1d8a4f
John Kelly <jkelly@incomm.com>
2022-03-11 14:05:13 -0600

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1 class="display-1">GCC Edit Replacement</h1>
+    <div class="row text-start ">
+      <div class="col-sm">Style Code</div>
+      <div class="col-sm">Style Name</div>
+      <div class="col-sm">Image Name</div>
+      <div class="col-sm">4th thing john forgets</div>
+    </div>
+    <div class="row  text-start ">
+      <div class="col-sm"><input class="form-control" type="text" value="000123"/></div>
+      <div class="col-sm">Not Stylish</div>
+      <div class="col-sm">Troll.png</div>
+      <div class="col-sm">TootsieRoll</div>
+    </div>
+    <button class="btn btn-outline-primary">Save</button>
+
+
+    <div class="row">
+      <div class="col">
+{{stuff}}
+      </div>
+    </div>
+    
+
+    
+
+  </div>
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+  },
+  data() {
+    return { stuff: [{sandwich: "subway"},{sandwich: "mike's"}]};
   }
 }
 </script>
